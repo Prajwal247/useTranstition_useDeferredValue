08e0f986771ed5878cb783e8584b0f8ac3f64c55
Prajwal247 <prazzwalthapa87@gmail.com>
2022-12-05 12:15:36 +0545

    fianl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useDeferredValue, useState, useTransition } from "react";
 
 function App() {
+  const [isPending, startTransition] = useTransition();
+  const [images, setImages] = useState(null);
+
+  const loadData = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((data) => setImages(data))
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div style={{ "text-align": "center", "margin":"50px 50px", "padding":"10px 20px" }}>
+        <button
+          onClick={() => {
+            startTransition(() => {
+              loadData();
+            });
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Display Color's box
+        </button>
+      </div>
+      <div
+        style={{
+          opacity: isPending ? 0.5 : 1,
+        }}
+      >
+        <DisplayImages imgs={images} />
+      </div>
+    </>
   );
 }
+
+const DisplayImages = ({ imgs }) => {
+  // for the implementation of useDeferredValue
+
+  // const deferredImages = useDeferredValue(imgs);
+  for (var a = [], i = 0; i < 10000; ++i) a[i] = i;
+  const getIndex = (index) => {
+    return index < 4999 ? index : index - 5000;
+  };
+
+  // for the implementation of useDeferredValue
+
+  // return (
+  //   <>
+  //     {deferredImages &&
+  //       a.map((item, index) => (
+  //         <img
+  //           src={deferredImages[getIndex(index)]?.url}
+  //           alt="LOGO"
+  //           style={{ height: 50, width: 50 }}
+  //           key={index}
+  //         />
+  //       ))}
+  //   </>
+  // );
+  return (
+    <>
+      {imgs &&
+        a.map((item, index) => (
+          <img
+            src={imgs[getIndex(index)]?.url}
+            alt="colors"
+            style={{ height: 20, width: 20 }}
+            key={index}
+          />
+        ))}
+    </>
+  );
+};
 
 export default App;
